cbe7f63f42e71ea9a1840d0f46ad1fadd10cdad2
Carlos Godinho <carlos.godinho@hotmail.com>
2020-06-01 11:09:44 -0300

    atualização token

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // replace these values with those generated in your TokBox Account
 var apiKey = "46612782";
-var sessionId = "2_MX40NjYxMjc4Mn5-MTU5MDE4MzUxNTcwNH5IbG84Sm1kQ2U2Y213UG9ib2VjRHBreWV-fg";
-var token = "T1==cGFydG5lcl9pZD00NjYxMjc4MiZzaWc9YTg3NjU4ZjlkNTQxYzgxYmM2MDBlMzE3ZGFmZjgwOTlkN2ZjMzJlZTpzZXNzaW9uX2lkPTJfTVg0ME5qWXhNamM0TW41LU1UVTVNREU0TXpVeE5UY3dOSDVJYkc4NFNtMWtRMlUyWTIxM1VHOWliMlZqUkhCcmVXVi1mZyZjcmVhdGVfdGltZT0xNTkwMTgzNTI1Jm5vbmNlPTAuNDQ1NzY4NDA3MzM4MTgwOSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTkwMjA1MTI0JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
+var sessionId = "1_MX40NjYxMjc4Mn5-MTU5MTAxOTA1OTk4Nn5SamhMTUpwWHJoTGxaNDdEaGpyajRpdmd-fg";
+var token = "T1==cGFydG5lcl9pZD00NjYxMjc4MiZzaWc9MzQ3N2YzYmY4ZTQxZTg4NTA5NzY1ZjYxMDI4ZDM1MmIzZGRkODE1ZTpzZXNzaW9uX2lkPTFfTVg0ME5qWXhNamM0TW41LU1UVTVNVEF4T1RBMU9UazRObjVTYW1oTVRVcHdXSEpvVEd4YU5EZEVhR3B5YWpScGRtZC1mZyZjcmVhdGVfdGltZT0xNTkxMDIwNTQ0Jm5vbmNlPTAuNzU4OTY5NjU5MzAyNDY3MyZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTkxMTA2OTQyJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
 
 // (optional) add server code here
 initializeSession();
@@ -42,47 +42,3 @@ function handleError(error) {
     });
   }
   // replace these values with those generated in your TokBox Account
-  var apiKey = "46612782";
-  var sessionId = "2_MX40NjYxMjc4Mn5-MTU5MDE4MzUxNTcwNH5IbG84Sm1kQ2U2Y213UG9ib2VjRHBreWV-fg";
-  var token = "T1==cGFydG5lcl9pZD00NjYxMjc4MiZzaWc9YTg3NjU4ZjlkNTQxYzgxYmM2MDBlMzE3ZGFmZjgwOTlkN2ZjMzJlZTpzZXNzaW9uX2lkPTJfTVg0ME5qWXhNamM0TW41LU1UVTVNREU0TXpVeE5UY3dOSDVJYkc4NFNtMWtRMlUyWTIxM1VHOWliMlZqUkhCcmVXVi1mZyZjcmVhdGVfdGltZT0xNTkwMTgzNTI1Jm5vbmNlPTAuNDQ1NzY4NDA3MzM4MTgwOSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTkwMjA1MTI0JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
-  
-
-// Handling all of our errors here by alerting them
-function handleError(error) {
-  if (error) {
-    alert(error.message);
-  }
-}
-
-// (optional) add server code here
-initializeSession();
-
-function initializeSession() {
-  var session = OT.initSession(apiKey, sessionId);
-
-  // Subscribe to a newly created stream
-  session.on('streamCreated', function(event) {
-    session.subscribe(event.stream, 'subscriber', {
-      insertMode: 'append',
-      width: '100%',
-      height: '100%'
-    }, handleError);
-  });
-
-  // Create a publisher
-  var publisher = OT.initPublisher('publisher', {
-    insertMode: 'append',
-    width: '100%',
-    height: '100%'
-  }, handleError);
-
-  // Connect to the session
-  session.connect(token, function(error) {
-    // If the connection is successful, initialize a publisher and publish to the session
-    if (error) {
-      handleError(error);
-    } else {
-      session.publish(publisher, handleError);
-    }
-  });
-}
